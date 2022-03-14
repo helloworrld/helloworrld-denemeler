77c9b21f4a78605d059563dd9ae65e1050c9ddfd
helloworrld <33907771+helloworrld@users.noreply.github.com>
2022-03-15 01:17:05 +0300

    html-css-denemeler

---
+++ Landing Page/app.js
@@ -0,0 +1,8 @@
+function warning(){
+    document.getElementById('warn').style.color = "red";
+    document.getElementById('warn').innerHTML = 'Aradağınız bulunamadı.';
+}
+
+function search(){
+    document.getElementById('warn').style.display = "none";
+}
